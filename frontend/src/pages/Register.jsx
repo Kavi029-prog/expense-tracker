4964e428c603cb7ad0a9c2e89292e200be539e03
KAVI VARSHINI K <24cs029@jayshriram.edu.in>
2026-06-05 19:49:43 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,3 +1,5 @@
+import API_URL from "../config";
+
 import { useState } from "react"
 
 import axios from "axios"
@@ -24,7 +26,7 @@ function Register() {
     try {
 
       await axios.post(
-        "http://127.0.0.1:8000/register",
+        `${API_URL}/register`,
 
         {
           username,
